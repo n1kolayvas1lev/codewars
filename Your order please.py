f90a187037359776d854da2e7f51c82df53d5507
n1kolayvas1lev <ckb01@ya.ru>
2022-04-03 19:55:49 +0300

    6.init

--- Your order please.py
+++ Your order please.py
@@ -9,7 +9,7 @@
 #
 # "is2 Thi1s T4est 3a"  -->  "Thi1s is2 3a T4est"
 # "4of Fo1r pe6ople g3ood th5e the2"  -->  "Fo1r the2 g3ood 4of th5e pe6ople"
-# ""  -->  ""
+# https://www.codewars.com/kata/55c45be3b2079eccff00010f/train/python
 def order(sentence: str) -> str:
     output_str = ''
     if len(sentence) > 0:
