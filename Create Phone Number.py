bf5360dfbe5101ab117a700a56b5b07391e8a148
n1kolayvas1lev <ckb01@ya.ru>
2022-04-08 18:08:02 +0300

    9

---
+++ Create Phone Number.py
@@ -0,0 +1,21 @@
+# Write a function that accepts an array of 10 integers (between 0 and 9),
+# that returns a string of those numbers in the form of a phone number.
+# Example
+# create_phone_number([1, 2, 3, 4, 5, 6, 7, 8, 9, 0]) # => returns "(123) 456-7890"
+# The returned format must be correct in order to complete this challenge.
+# Don't forget the space after the closing parentheses!
+def create_phone_number(phone_number: list) -> str:
+    phone_number.insert(0, '(')
+    phone_number.insert(4, ')')
+    phone_number.insert(5, ' ')
+    phone_number.insert(9, '-')
+    phone_number = ''.join(str(i) for i in phone_number)
+    return phone_number
+
+
+if __name__ == "__main__":
+    create_phone_number([1, 2, 3, 4, 5, 6, 7, 8, 9, 0])  # "(123) 456-7890"
+    create_phone_number([1, 1, 1, 1, 1, 1, 1, 1, 1, 1])  # "(111) 111-1111"
+    create_phone_number([1, 2, 3, 4, 5, 6, 7, 8, 9, 0])  # "(123) 456-7890"
+    create_phone_number([0, 2, 3, 0, 5, 6, 0, 8, 9, 0])  # "(023) 056-0890"
+    create_phone_number([0, 0, 0, 0, 0, 0, 0, 0, 0, 0])  # "(000) 000-0000"
